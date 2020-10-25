ed3420137acd7846766ada4d48d14b0a3ed227b2
Phy Lieng <phylieng97@gmail.com>
2020-10-25 21:48:29 +0700

    feat<tmp_form> add temp form

--- lib/screens/UserLogin/UserLogin.dart
+++ lib/screens/UserLogin/UserLogin.dart
@@ -6,6 +6,67 @@ class UserLogin extends StatefulWidget {
 }
 
 class _UserLoginState extends State<UserLogin> {
+  TextEditingController nameController;
+  TextEditingController emailController;
+  TextEditingController passwordController;
+
+  Column buildInputContainer(
+      BuildContext context, TextEditingController controller, String title) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title, style: Theme.of(context).textTheme.bodyText1),
+        TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            icon: Icon(Icons.person),
+            hintText: 'What do people call you?',
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    nameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  Future<void> showInfoDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                buildInputContainer(context, nameController, "Name"),
+                buildInputContainer(context, emailController, "Email"),
+                buildInputContainer(context, passwordController, "Password")
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Approve'),
+              onPressed: () {
+                // FocusScope.of(context).unfocus();
+                FocusManager.instance.primaryFocus.unfocus();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -34,7 +95,19 @@ class _UserLoginState extends State<UserLogin> {
                                   Expanded(
                                       flex: 4,
                                       child: Container(
-                                          color: Colors.orangeAccent)),
+                                          padding: EdgeInsets.zero,
+                                          color: Colors.orangeAccent,
+                                          child: MaterialButton(
+                                            color: Colors.white,
+                                            shape: CircleBorder(),
+                                            child: Icon(
+                                              Icons.add,
+                                              size: 24,
+                                            ),
+                                            onPressed: () {
+                                              showInfoDialog();
+                                            },
+                                          ))),
                                   Expanded(
                                       flex: 1,
                                       child: Container(
